fa5423607271e502188a9d89e95d1954fadb3485
Ali Sajad Enayat <alisajadenayat2222@gmail.com>
2025-11-02 17:54:44 +0100

    Update test.js

--- test.js
+++ test.js
@@ -8,3 +8,17 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+
+(function() {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.querySelector("header").style.top = "0";
+    } else {
+      document.querySelector("header").style.top = "-130px";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+})();
